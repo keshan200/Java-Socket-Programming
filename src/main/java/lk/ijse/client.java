f882499e34d2a54c399d8fc0c2e829f040390cf1
keshan200 <sachithakeshan175@gmail.com>
2024-10-14 09:50:22 +0530

    ssd

--- src/main/java/lk/ijse/client.java
+++ src/main/java/lk/ijse/client.java
@@ -46,3 +46,43 @@ public class client {
 
     }
 }
+
+
+/*package lk.ijse;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost" , 3000);
+
+            while (!socket.isClosed()){
+                Scanner input = new Scanner(System.in);
+
+                System.out.println("Client :");
+                String messege =input.next();
+                DataOutputStream dataOutStream = new DataOutputStream(socket.getOutputStream());
+                dataOutStream.writeUTF(messege);
+                dataOutStream.flush();
+
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                String rep = dataInputStream.readUTF();
+                System.out.println("Server :"+rep);
+
+            }
+            socket.close();
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}*/
--- src/main/java/lk/ijse/server.java
+++ src/main/java/lk/ijse/server.java
@@ -47,3 +47,47 @@ public class server {
         }
     }
 }
+
+/*
+package lk.ijse;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class server {
+    public static void main(String[] args) {
+        try {
+            System.out.println("waiting for client");
+            ServerSocket serverSocket = new ServerSocket(3000);
+            Socket socket = serverSocket.accept();
+
+            while (!socket.isClosed()){
+                System.out.println("Client accepted");
+
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+                String msg = dataInputStream.readUTF();
+                System.out.println("Client " + msg);
+
+
+                Scanner input = new Scanner(System.in);
+                System.out.println("Server :");
+                String mm =input.next();
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeUTF(mm);
+                dataOutputStream.flush();
+
+            }
+            socket.close();
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}*/
